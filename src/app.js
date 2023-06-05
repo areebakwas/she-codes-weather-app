f591cbef5c8bc48826f138ca534a7f823c20a86c
areebakwas <32884881+areebakwas@users.noreply.github.com>
2023-06-05 14:49:03 -0400

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -37,8 +37,22 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-
+function search(city) {
 let apiKey = "ff87b4194fd01246b591804c7dcb46e5";
-let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+}
+
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let inputCityElement = document.querySelector("#city-input");
+    search(inputCityElement.value);
+    
+}
+
+search("New York");
+
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", handleSubmit);
