122ef39fd5d248fa279da6f46536e4bda7664e7e
areebakwas <32884881+areebakwas@users.noreply.github.com>
2023-06-06 06:50:45 -0400

    implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -26,6 +26,8 @@ function displayTemperature(response) {
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
 
+    celciusTemperature = response.data.main.temp;
+
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
@@ -52,7 +54,33 @@ function handleSubmit(event) {
     
 }
 
-search("New York");
+function diplayFahrenheitTemp(event) {
+    event.preventDefault();
+    celciusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celciusTemperature * 9/5) + 32 ;
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelciusTemp(event) {
+    event.preventDefault();
+    celciusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celciusTemperature);
+}
+
+let celciusTemperature = null;
+
 
   let form = document.querySelector("#search-form");
   form.addEventListener("submit", handleSubmit);
+
+  let fahrenheitLink = document.querySelector("#fahrenheit-link");
+  fahrenheitLink.addEventListener("click", diplayFahrenheitTemp);
+
+  let celciusLink = document.querySelector("#celcius-link");
+  celciusLink.addEventListener("click", displayCelciusTemp);
+
+  search("New York");
